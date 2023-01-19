71a7c116839bfae601c6c55e19df36f58d2bd5bd
Ken769789 <alex@auco.co.nz>
2023-01-19 14:27:36 +1000

    upload

--- js/script.js
+++ js/script.js
@@ -1,107 +1,148 @@
 const addTripBtn = document.getElementById('add-trip-btn');
 const tripContainer = document.getElementById('trip-container');
 const goBtn = document.getElementById('go-btn');
-const result = document.getElementById('result');
-// const trip_1 = document.getElementById('trip-1');
-// const passenger_1 = document.getElementById('passenger-1');
-
-// const tripData = document.querySelectorAll('trip-data');
-
-// tripData.forEach(el => el.addEventListener('click', event => {
-//   console.log(event.target.getAttribute("data-el"));
-// }));
-// document.body.addEventListener('click', function (evt) {
-//     if (evt.target.className === 'trip-data') {
-//         alert(this.);
-//     }
-// }, false);
-
-let trip = 1;
-
-//create first trip choice
-let trips = [
-    { race_number: trip, passenger: 'undefined', relation: 'undefined', river_side: 0 }
+const resultMsg = document.getElementById('result-msg');
+const tripData = document.getElementById('trip-data');
+const passengerData = document.getElementById('passenger-data')
+const resultSaveBtn = document.getElementById('save-btn')
+let rightResult = [
+    { race_number: 1, wolf: 0, goat: 'правый берег', cabbege: 0 },
+    { race_number: 2, wolf: 'правый берег', goat: 'правый берег', cabbege: 0 },
+    { race_number: 3, wolf: 'правый берег', goat: 'левый берег', cabbege: 0 },
+    { race_number: 4, wolf: 'правый берег', goat: 'левый берег', cabbege: 'правый берег' },
+    { race_number: 5, wolf: 'правый берег', goat: 'правый берег', cabbege: 'правый берег' },
 ]
 
+//list of trips
+let trips = []
+let trip = 1;
+let riverSide = 'Выбрать рейс';
+let wolfPosition = 0
+let goatPostion = 0
+let cabbegePosition = 0
+let passangerName = 'undefined'
+let reportMsg
+
+//enter direction
+tripData.addEventListener('click', () => {
+    riverSide = tripData.value;
+    userMsg('clear')
+})
+
+//enter passanger
+passengerData.addEventListener('click', () => {
+    passangerName = passengerData.value;
+    userMsg('clear')
+})
+
+//run the test
+goBtn.addEventListener('click', () => {
+    if (trips != 0) {
+        checkResult()
+    }
+})
+
+//add the trip to array
 addTripBtn.addEventListener('click', () => {
-    trip++
+
+    if (riverSide === 'Выбрать рейс' || passangerName === 'undefined') {
+        userMsg('fillup')
+        return false
+    }
+    switch (passangerName) {
+        case 'волк':
+            wolfPosition = riverSide
+            break;
+        case 'коза':
+            goatPostion = riverSide
+            break;
+        case 'капуста':
+            cabbegePosition = riverSide
+            break;
+        default:
+            break;
+    }
+
+    trips.push({ race_number: trip, wolf: wolfPosition, goat: goatPostion, cabbege: cabbegePosition })
+
     let nextTrip = document.createElement('div');
     nextTrip.className = "trip"
-    nextTrip.innerHTML = "<div class='trip-name'>Рейс " + trip + "</div>"
-        + "<select class='trip-data space-selection'>"
-        + "<option>Выбрать рейс</option>"
-        + "<option value='1'>с лева на права</option>"
-        + "<option value='2'>с права на лева</option></select >"
-        + "<select id='passenger-" + trip + "' class='space-selection'>"
-        + "<option>Выбрать пассажира</option>"
-        + "<option value='1'>Волк</option>"
-        + "<option value='2'>Коза</option>"
-        + "<option value='3'>Капуста</option></select>";
+    nextTrip.innerHTML = "<div class='trip-name'>рейс " + trip + "</div>"
+        + "<div class='trip-line'>" + riverSide + "</div>"
+        + "<div class='trip-line'>" + passangerName + "</div>";
     tripContainer.appendChild(nextTrip)
-    trips.push({ race_number: trip, passenger: 'undefined', relation: 'undefined', river_side: 0 }) 
+    // next trip
+    trip++
+    // set to default
+    riverSide = 'Выбрать рейс'
+    passangerName = 'undefined'
+
+    console.log(trips)
 })
 
-let temp;
-document.querySelectorAll('.trip-data').forEach(item => {
-    item.addEventListener('click', event => {
-     temp = item.options[item.selectedIndex].value;
-    //  alert(temp)
-    })
-  })
-// const arrClass = document.querySelectorAll(".trip-data");
-// for (let item of arrClass) {
-//     item.addEventListener('click', event => {
-//              temp = item.options[item.selectedIndex].value;
-//             })
-//   }
-// trip_1.addEventListener('click', () => {
-//     trips[0].river_side = trip_1.value;
-// })
-
-// passenger_1.addEventListener('click', () => {
-//     trips[0].passenger = passenger_1.value;
-//     switch (passenger_1.value) {
-//         case 'wolf':
-//             trips[0].relation = 'goat'
-//             break;
-//         case 'goat':
-//             trips[0].relation = 'cabbage'
-//             break;
-//         case 'cabbage':
-//             trips[0].relation = 'goat'
-//             break;
-//         default:
-//             break;
-//     }
-// })
-
-goBtn.addEventListener('click', () => {
-    // checkResult()
-    // alert(trips[0].river_side + trips[0].relation + "   " + trips[trip-1].river_side + trips[trip-1].relation)
-    alert(temp)
-})
 
 function checkResult() {
-    //wc -> g
-    //c -> wg
-    //cg <- w
-    //g ->cw
-    // ->gcw
-    if (trip < 5) {
-        return wrong()
+    if (JSON.stringify(trips) === JSON.stringify(rightResult)) {
+        userMsg('won')
+        reportMsg = resultMsg.innerText
+    } else {
+        userMsg('lost')
+        reportMsg = resultMsg.innerText
     }
-    return correct()
+    resultSaveBtn.className = "button orange"
+}
+
+
+//run the test
+resultSaveBtn.addEventListener('click', () => {
+    userMsg('filesave')
+    saveResult()
+})
+
+function saveResult() {
+
+    let gameResult;
+
+    trips.forEach((item, index) => {
+        gameResult += "шаг > " + item['race_number']
+            + " | позиция волка > " + item['wolf']
+            + " | позиция козы > " + item['goat']
+            + " | позиция капусты > " + item['cabbege'] + '\n'
+    })
+
+    gameResult += "Результат > " + reportMsg
+
+    var saveResult = new Blob([gameResult], { type: "text/plain;charset=utf-8" });
+    saveAs(saveResult, "result.txt");
 }
 
 
 
 
-function correct() {
-    result.className = "result right_answer"
-    result.innerText = "Ура! Правильный ответ!"
+function userMsg(situation) {
+
+    switch (situation) {
+        case 'won':
+            resultMsg.className = "result right_answer"
+            resultMsg.innerText = "Ура! Правильный ответ!"
+            break;
+        case 'lost':
+            resultMsg.className = "result wrong_answer"
+            resultMsg.innerText = "Ответ неверный. Нужно подумать!"
+            break;
+        case 'fillup':
+            resultMsg.className = "result wrong_answer"
+            resultMsg.innerText = "Выберите берег куда перевозить и кого!"
+            break;
+        case 'filesave':
+            resultMsg.className = "result right_answer"
+            resultMsg.innerText = "Ваш результат сохранён в файле."
+            break;
+        default:
+            resultMsg.className = ""
+            resultMsg.innerText = ""
+            break;
+    }
 }
 
-function wrong() {
-    result.className = "result wrong_answer"
-    result.innerText = "Нужно подумать!"
-}
+
